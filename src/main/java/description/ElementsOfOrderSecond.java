3a71762cefe00834ace690b3140a76b521822f60
Julia <julchik.94@yandex.ru>
2022-07-12 07:22:47 +0300

    Elements of Main Page and First Page Order

--- src/main/java/description/ElementsOfOrderSecond.java
+++ src/main/java/description/ElementsOfOrderSecond.java
@@ -1,4 +1,21 @@
 package description;
 
 public class ElementsOfOrderSecond {
+    //Поле Когда привезти самокат
+    //   .//input[@placeholder='* Когда привезти самокат']
+
+    //Поле Срок аренды
+    //  .//div[@class='Dropdown-placeholder' and text() = '* Срок аренды']
+
+    //Цвет: Чекбокс "Черный жемчуг"
+    //  id = "black"
+
+    //Цвет: Чекбокс "Серая безысходность"
+    //  id = "grey"
+
+    //Поле "Комментарий"
+    //   .//input[@placeholder='Комментарий для курьера']
+
+    //Кнопка Заказать
+    //   .//button[@class ='Button_Button__ra12g Button_Middle__1CSJM' and text() = 'Заказать']
 }
---
+++ src/main/java/description/ElementsOfPopUp.java
@@ -0,0 +1,9 @@
+package description;
+
+public class ElementsOfPopUp {
+    //Кнопка Да
+    //   .//button[@class ='Button_Button__ra12g Button_Middle__1CSJM' and text() = 'Да']
+
+    //Текст "Заказ оформлен"
+    //class="Order_ModalHeader__3FDaJ"
+}
